481e945acb2a13c86e01d96d2a7cdb4a1dd8e1c8
Noah Rubin <95647477+haonnoah123@users.noreply.github.com>
2022-05-16 10:12:38 -0500

    Initial Commit

---
+++ CalculusApp/CalculusAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculusAppApp.swift
+//  CalculusApp
+//
+//  Created by 90309333 on 5/16/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculusAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
